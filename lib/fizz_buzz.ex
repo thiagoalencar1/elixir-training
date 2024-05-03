7835a3c002a6af0260b37257ef77cdf515e9c7f0
Thiago Alencar <talencar@gmail.com>
2024-05-03 09:29:51 -0300

    feat: implement fizbuzz #2

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,18 +1,24 @@
 defmodule FizzBuzz do
-  @moduledoc """
-  Documentation for `FizzBuzz`.
-  """
+  ### Utilizando o case
+  # def build(filename) do
+  #   case File.read(filename) do
+  #     {:ok, result} -> result
+  #     {:error, reason} -> reason
+  #   end
+  # end
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> FizzBuzz.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  ### Utilizando funções
+  def build(filename) do
+    filename
+    |> File.read()
+    |> handle_file_read()
   end
+
+  def handle_file_read({:ok, result}) do
+    result
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
 end
