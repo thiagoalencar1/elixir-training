71ecbd79a0d755ec00707ec5647e32c1b182f2ac
Thiago Alencar <talencar@gmail.com>
2024-05-07 21:25:01 -0300

    feat: Add computer turn

--- exmon/lib/ex_mon.ex
+++ exmon/lib/ex_mon.ex
@@ -4,6 +4,7 @@ defmodule ExMon do
   alias ExMon.Game.{Actions, Status}
 
   @computer_name "Robotinik"
+  @computer_moves [:move_avg, :move_rnd, :move_heal]
 
   # Aqui é definida a função create_player, que basicamente permite que criemos um Player direto do módulo ExMon
   def create_player(name, move_rnd, move_avg, move_heal) do
@@ -19,12 +20,23 @@ defmodule ExMon do
   end
 
   def make_move(move) do
+    Game.info()
+    |> Map.get(:status)
+    |> handle_status(move)
+
+    computer_move(Game.info())
+  end
+
+  defp handle_status(:game_over, _move), do: Status.print_round_message(Game.info())
+
+  defp handle_status(_other, move) do
     move
     |> Actions.fetch_move()
     |> do_move()
   end
 
   defp do_move({:error, move}), do: Status.print_wrong_move_message(move)
+
   defp do_move({:ok, move}) do
     case move do
       :move_heal -> Actions.heal()
@@ -33,4 +45,11 @@ defmodule ExMon do
 
     Status.print_round_message(Game.info())
   end
+
+  defp computer_move(%{turn: :computer, status: :continue}) do
+    move = {:ok, Enum.random(@computer_moves)}
+    do_move(move)
+  end
+
+  defp computer_move(_), do: :ok
 end
