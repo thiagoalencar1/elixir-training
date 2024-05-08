4ce0d03ba97f9cc27dbdf6da1432ccdfba6c9ee8
Thiago Alencar <talencar@gmail.com>
2024-05-08 14:44:52 -0300

    feat: Add criptography to User password

--- banana_bank/mix.exs
+++ banana_bank/mix.exs
@@ -41,7 +41,8 @@ defmodule BananaBank.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:argon2_elixir, "~> 4.0"}
     ]
   end
 
