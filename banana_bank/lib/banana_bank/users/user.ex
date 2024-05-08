01b62b4e2fefb1aa8d215c393cb010791f0dc223
Thiago Alencar <talencar@gmail.com>
2024-05-08 10:54:46 -0300

    feat: Improve User changeset

--- banana_bank/lib/banana_bank/users/user.ex
+++ banana_bank/lib/banana_bank/users/user.ex
@@ -2,6 +2,8 @@ defmodule BananaBank.Users.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @required_params [:name, :password_hash, :email, :cep]
+
   schema "users" do
     field :name,          :string
     field :password_hash, :string
@@ -13,7 +15,10 @@ defmodule BananaBank.Users.User do
 
   def changeset(user \\ %__MODULE__{}, params) do
     user
-    |> cast(params, [:name, :password_hash, :email, :cep])
-    |> validate_required([:name, :password_hash, :email, :cep])
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:name, min: 3)
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:cep, is: 8)
   end
 end
