6ea4303ac4aeedb6f4affca7f36d3308da952809
Thiago Alencar <talencar@gmail.com>
2024-05-08 21:13:35 -0300

    feat: Add Show action to users

--- banana_bank/lib/banana_bank_web/controllers/users_json.ex
+++ banana_bank/lib/banana_bank_web/controllers/users_json.ex
@@ -1,11 +1,14 @@
 defmodule BananaBankWeb.UsersJSON do
   alias BananaBank.Users.User
+
   def create(%{user: user}) do
     %{
       message: "User criado com sucesso",
       data: data(user)
     }
   end
+
+  def get(%{user: user}), do: %{data: data(user)}
 
   defp data(%User{} = user) do
     %{
