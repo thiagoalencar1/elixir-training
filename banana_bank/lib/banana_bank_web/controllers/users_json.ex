af5b768042a34e8416859d25016c2ee9a77c009d
Thiago Alencar <talencar@gmail.com>
2024-05-08 22:08:53 -0300

    feat: Add Update action to Users

--- banana_bank/lib/banana_bank_web/controllers/users_json.ex
+++ banana_bank/lib/banana_bank_web/controllers/users_json.ex
@@ -3,12 +3,14 @@ defmodule BananaBankWeb.UsersJSON do
 
   def create(%{user: user}) do
     %{
-      message: "User criado com sucesso",
+      message: "User criado com sucesso.",
       data: data(user)
     }
   end
 
   def get(%{user: user}), do: %{data: user}
+
+  def update(%{user: user}), do: %{message: "User atualizado com sucesso.", data: user}
 
   defp data(%User{} = user) do
   %{
