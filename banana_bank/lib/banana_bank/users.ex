0a67d4b79e6580197a99ed6460e324aba5373548
Thiago Alencar <talencar@gmail.com>
2024-05-08 21:12:57 -0300

    feat: Add Users Facade

---
+++ banana_bank/lib/banana_bank/users.ex
@@ -0,0 +1,8 @@
+# This is an Facade to User
+defmodule BananaBank.Users do
+  alias BananaBank.Users.Create
+  alias BananaBank.Users.Get
+
+  defdelegate create(params), to: Create, as: :call
+  defdelegate get(id),        to: Get,    as: :call
+end
