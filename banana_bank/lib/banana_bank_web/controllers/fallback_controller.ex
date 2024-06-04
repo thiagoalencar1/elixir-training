71e4194ed81c14e2925c68afc297706dcb983e86
Thiago Alencar <talencar@gmail.com>
2024-06-04 16:40:42 -0300

    feat: Create ViaCep client and errors matches

--- banana_bank/lib/banana_bank_web/controllers/fallback_controller.ex
+++ banana_bank/lib/banana_bank_web/controllers/fallback_controller.ex
@@ -3,9 +3,16 @@ defmodule BananaBankWeb.FallbackController do
 
   def call(conn, {:error, :not_found}) do
     conn
-    |> put_status(:bad_request)
+    |> put_status(:not_found)
     |> put_view(json: BananaBankWeb.ErrorJSON)
     |> render(:error, status: :not_found)
+  end
+
+  def call(conn, {:error, :bad_request}) do
+    conn
+    |> put_status(:bad_request)
+    |> put_view(json: BananaBankWeb.ErrorJSON)
+    |> render(:error, status: :bad_request)
   end
 
   def call(conn, {:error, changeset}) do
